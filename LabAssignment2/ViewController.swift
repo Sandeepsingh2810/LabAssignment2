8dd20c892384dd123bc1bea029c76ee968422a8b
Karan <kumarkay.ca@gmail.com>
2020-01-21 20:17:54 -0500

    Initial Commit

---
+++ LabAssignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LabAssignment2
+//
+//  Created by Sandeep Jangra on 2020-01-21.
+//  Copyright © 2020 Sandeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
